f5387c63ff1262440601f0cb24bee5d870de3b12
upperxcode <joaoalbuquerque.br@gmail.com>
2017-12-22 02:44:36 -0200

    message

---
+++ index.js
@@ -0,0 +1,32 @@
+class Message {
+  constructor (text = '', created = Date.now(), msg = ''){
+    this.text    = text;
+    this.created = created;
+    this._msg     = msg;
+  }
+
+  get msg(){
+    return this.msg;
+  }
+
+  set msg(msg){
+
+    if (!msg || isNaN(msg)) {
+      throw new Error('Invalid Msg - Mensagem Inválida!');
+    }
+
+
+
+    this._msg = msg;
+  }
+
+  toString(){
+    return `Message ${this.created} - Text: ${this.text} message ${this._msg}`;
+  }
+}
+
+var emptyMessage = new Message('',Date.now(), 'dddd');
+var textMessage  =  new Message('Minha messagem', Date.now() - 86400);
+
+console.log(String(emptyMessage));
+console.log(String(textMessage));
